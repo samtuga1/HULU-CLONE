17c424dd6e6b2a6e3440e7fbac3f0f145c676132
Samuel <samueltwumasi1290.com>
2023-02-23 15:31:55 +0000

    final updates

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div>
+    <div className="sm:grid px-5 my-10 md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 function Thumbnail({ result }) {
   const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
+    <div className="group cursor-pointer p-2 transition duration-200 transform sm:hover:scale-105 hover:z-50">
       <Image
         height={1080}
         width={1920}
@@ -17,8 +17,10 @@ function Thumbnail({ result }) {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p className="flex items-center">
+        <h2 className="mt-1 text-2xl text-white group-hover:font-bold transition-all duration-200 ease-in-out">
+          {result.title || result.original_name}
+        </h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100 cursor-default">
           {result.media_type && `${result.media_type} • `}
           {result.release_date || `${result.first_air_date} • `}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
